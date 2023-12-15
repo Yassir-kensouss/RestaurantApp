50a4e6811e76191d4c2017a58bb1849dc1e104af
Yassir-kensouss <kensoussyassir@gmail.com>
2023-12-14 20:59:03 -0500

    Add project files.

---
+++ Restaurant.Model/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Restaurant.Model
+{
+    public class Category
+    {
+        [Key] // Primary key
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Category name is required")]
+        [MaxLength(100, ErrorMessage = "Category name has to be 100 char or less")]
+        [Display(Name = "Category_Name")]
+        public string? Name { get; set; }
+
+
+        [Display(Name = "Display Order")]
+        [Required(ErrorMessage = "Display order is required")]
+        [Range(1,200, ErrorMessage = "Display order must be between 1 and 200")]
+        public int DisplayOrder { get; set; }
+    }
+}
---
+++ RestaurantDAL/RestaurantDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.Model;
+
+namespace RestaurantDAL
+{
+    public class RestaurantDBContext : DbContext
+    {
+        public RestaurantDBContext(DbContextOptions<RestaurantDBContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
